8b01bbbd0627b5e2af5d49ef7ddb8881173d87ca
HT4w5 <ht4w5x@gmail.com>
2025-11-13 19:11:51 +0800

    config: read from JSON

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/HT4w5/bgpsim-go
+
+go 1.24.9
---
+++ internal/pkg/config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/HT4w5/bgpsim-go/internal/pkg/model"
+)
+
+// Primary config struct
+type Config struct {
+	Nodes       []*model.NodeConfig      `json:"nodes"`
+	BgpTopology *model.BgpTopologyConfig `json:"bgpTopology"`
+}
+
+// Create empty config
+func New() *Config {
+	return &Config{
+		Nodes: []*model.NodeConfig{},
+		BgpTopology: &model.BgpTopologyConfig{
+			Nodes: []*model.BgpNodeConfig{},
+			Edges: []*model.BgpEdgeConfig{},
+		},
+	}
+}
+
+// Load config from JSON file
+func (c *Config) Load(path string) error {
+	configBytes, err := os.ReadFile(path)
+	if err != nil {
+		return fmt.Errorf("failed to open file %s: %w", path, err)
+	}
+
+	if err := json.Unmarshal(configBytes, c); err != nil {
+		return fmt.Errorf("failed to unmarshal json config: %w", err)
+	}
+
+	return nil
+}
